1427e7cce79763ac9ad66adba6b64d05a005e9e1
Kabunda <kbobah@yandex.ru>
2025-01-18 21:54:36 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,12 @@
 function print_string(num) {
 	for (var i = 0; i < 37; i++) {
 		document.getElementById(i).innerHTML = i
-		//console.log(i)
 		document.getElementById(i).style.color = f(i)
-		if (num.some(i)) {
-			document.getElementById(i).style.color = "blue"
-		}
+		  if (num.some(cur => cur == i)) {
+		      	document.getElementById(i).style.color = "yellow"
+		  }
 	}
 }
-
 function even_odd(num) {
     if (num == 0) return 0
     if (num % 9 == 0)
@@ -16,7 +14,6 @@ function even_odd(num) {
     else
         return num % 9
 }
-
 function f(num) {
     //green, black, red = 'зеленое', 'черное', 'красное'
     if (num == 0) return 'green'
@@ -27,8 +24,9 @@ function f(num) {
 		return 'black'
 }
 function multi(){
-	tmp = document.getElementById('name').value)
-	print_string(neibors(tmp)) 
+	tmp = document.getElementById('name').value
+    tmp1 = neibors(tmp)
+	print_string(tmp1) 
 }
 function neibors(num){
 const wr = [0,
@@ -38,17 +36,15 @@ const wr = [0,
                   5, 24, 16, 33, 1, 20,
                   14, 31, 9, 22, 18, 29,
                   7, 28, 12, 35, 3, 26]
-//console.log (wheel_roulette[1]) 
  ans = []
-    nid = wr.indexOf(num)
-    ans. push(wr[nid - 2])
-    ans. push(wr[nid - 1])
-    ans. push(wr[nid])
-    ans. push(wr[nid + 1])
-    ans. push(wr[nid + 2])
+    nid = wr.indexOf(Number(num))
+    ans.push(wr.at(nid - 2))
+    ans.push(wr.at(nid - 1))
+    ans.push(wr[nid])
+    ans.push(wr[(nid < 36 ? nid + 1 : 0)])
+    ans.push(wr[(nid < 35 ? nid + 2 : (nid == 35 ? 0 : 1))])
  return ans            
 }
- //console.log(neibors(11) )
-print_string()
+print_string([37,37])
 
 
