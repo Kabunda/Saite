14d9068075d6e64115ee05aa8fa37b841530910f
Kabunda <kbobah@yandex.ru>
2025-01-31 22:43:36 +0300

    Запись тестового файла

--- scripts/ftJS.js
+++ scripts/ftJS.js
@@ -26,3 +26,68 @@ control.addEventListener("change", function(event) {
     console.log(reader.result);
 }, false);
 
+const filePicker$ = document.querySelector('.file-picker')
+// настройки
+const options = {
+  // можно выбирать несколько файлов
+  multiple: true,
+  // разрешенный тип файлов
+  types: [
+    {
+      description: 'Text',
+      accept: {
+        'text/plain': '.txt'
+        // разрешаем изображения
+        // 'image/*': ['.jpg', '.jpeg', '.png', '.gif']
+      }
+    }
+  ],
+  // можно выбирать только разрешенные файлы
+  // по моим наблюдениям, данная настройка работает не совсем корректно
+  excludeAcceptAllOption: true
+}
+
+filePicker$.addEventListener('click', async () => {
+  const fileHandles = await window.showOpenFilePicker(options)
+
+  const allFilesContent = await Promise.all(
+    fileHandles.map(async (fileHandle) => {
+      const file = await fileHandle.getFile()
+      const fileContent = await file.text()
+      return fileContent
+    })
+  )
+
+  console.log(allFilesContent.join('\n'))
+})
+
+const fileSaver$ = document.querySelector('.file-saver')
+
+// настройки
+const optionss = {
+  // рекомендуемое название файла
+  suggestedName: 'test4.txt',
+  types: [
+    {
+      description: 'Text',
+      accept: {
+        'text/plain': '.txt'
+      }
+    }
+  ],
+  excludeAcceptAllOption: true
+}
+
+// данные для записи
+const fileData = 'Bye World Once Again'
+
+fileSaver$.addEventListener('click', async () => {
+  const fileHandle = await window.showSaveFilePicker(optionss)
+  const writableStream = await fileHandle.createWritable()
+
+  await writableStream.write(fileData)
+  // данный метод не упоминается в черновике спецификации,
+  // хотя там говорится о необходимости закрытия потока
+  // для успешной записи файла
+  await writableStream.close()
+})
